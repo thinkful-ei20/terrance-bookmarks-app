eaaf137798842d58215f28e5f69622324dbdc55e
Terrance Corley <terrancecorley@outlook.com>
2018-04-12 19:19:54 -0700

    added main structure for app

--- scripts/store.js
+++ scripts/store.js
@@ -1,1 +1,30 @@
 'use strict'; 
+
+/* global $ */
+
+// const store = function() {
+
+//   return {
+//     items: [],
+//     error: null,
+//     setError,
+//     addItem,
+//     findById,
+//     findAndDelete,
+//     renderState: {
+//       adding: true,
+//     }
+//   }
+
+
+// }
+
+// function addItem() {
+
+// }
+
+// function render() {
+//   if (store.renderState.adding) {
+
+//   }
+// }
