13c3a27e93c27f7aeb6f0ea3fc8fc181e46d6286
Terrance Corley <terrancecorley@outlook.com>
2018-04-12 22:31:42 -0700

    created api.js

--- scripts/api.js
+++ scripts/api.js
@@ -1,3 +1,39 @@
 'use strict'; 
 
 /* global $ */
+
+const api = (() => {
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/terrance';
+
+  const getItems = (callback) => {
+    $.getJSON(`${BASE_URL}/bookmarks`, callback);
+  }
+
+  const createItem = (item, onSuccess, onError) => {
+    const newItem = JSON.stringify(item);
+    $.ajax({
+      url: `${BASE_URL}/bookmarks`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: newItem,
+      success: onSuccess,
+      error: onError,
+    });
+  }
+
+  const deleteItem = (id, onSuccess, onError) => {
+    $.ajax({
+      url: `${BASE_URL}/bookmarks/${id}`;
+      method: 'DELETE',
+      success: onSuccess,
+      error: onError,
+    });
+  };
+
+  return {
+    getItems,
+    createItem,
+    deleteItem,
+  };
+  
+}());
